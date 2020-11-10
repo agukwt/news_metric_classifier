a4ac46e5b17d5d3289ee8d735d34321df54aea95
agukwt <agukwt@dummy.com>
2020-11-11 00:22:22 +0900

    データ分割とデータローダー化処理を追加

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import sys
 
+import torch
+from sklearn.model_selection import train_test_split
+
 from src.dataset import make_textdata_dict
 from src.dataset import make_contrastive_dataflame
 from src.dataset import ContrastiveDataSet
@@ -8,7 +11,6 @@ from src.text_transfomer import BertTransform
 
 
 def main():
-    # %%
     ##################################################
     # 1 データセット作成　　　　　
     ##################################################
@@ -18,14 +20,23 @@ def main():
     contrastive_df = make_contrastive_dataflame(textdata_dict, contrastive_num=500)
     # 対照情報に対する変換処理を規定
     transform = BertTransform()
-    # 対照情報を分割
-    # TODO: split
-    train_df = contrastive_df
-    test_df = contrastive_df
+    # 対照情報を分割(train:val:test= 6:2:2)
+    twenty_rasion_num = int(500 * 2 * 9 / 5)
+    train_eval_df, test_df = train_test_split(contrastive_df, test_size=twenty_rasion_num, random_state=0)
+    train_df, val_df = train_test_split(train_eval_df, test_size=twenty_rasion_num, random_state=0)
     # torchのDataSet化
     train_datasetx = ContrastiveDataSet(train_df, transform, phase='train')
+    val_datasetx = ContrastiveDataSet(train_df, transform, phase='test')
     test_datasetx = ContrastiveDataSet(test_df, transform, phase='test')
-    
+
+    ##################################################
+    # 2 データローダー作成　　　　　
+    ##################################################
+    batch_size = 32
+    train_dataloder = torch.utils.data.DataLoader(train_datasetx, batch_size=batch_size, shuffle=True)
+    val_dataloder = torch.utils.data.DataLoader(val_datasetx, batch_size=batch_size, shuffle=False)
+    test_dataloder = torch.utils.data.DataLoader(test_datasetx, batch_size=batch_size, shuffle=False)
+
     sys.exit('Finish')
 
 
